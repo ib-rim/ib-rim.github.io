6dd13c9d1e6e597a57f6dca398a2293511b8862c
ib-rim <ib.bashir@hotmail.com>
2022-04-14 18:03:19 +0100

    Refactored class and id names to use BEM

--- scripts.js
+++ scripts.js
@@ -1,13 +1,27 @@
 function showAbout() {
-    var aboutFolder = document.getElementById("aboutFolder");
-    var portfolioFolder = document.getElementById("portfolioFolder");
+    var aboutFolder = document.querySelector("#folder--about");
+    var portfolioFolder = document.querySelector("#folder--portfolio");
     aboutFolder.style.display = "flex";
     portfolioFolder.style.display = "none";
 }
-  
+
 function showPortfolio() {
-    var aboutFolder = document.getElementById("aboutFolder");
-    var portfolioFolder = document.getElementById("portfolioFolder");
+    var aboutFolder = document.querySelector("#folder--about");
+    var portfolioFolder = document.querySelector("#folder--portfolio");
     aboutFolder.style.display = "none";
     portfolioFolder.style.display = "flex";
 }
+
+document.addEventListener("DOMContentLoaded", function (event) {
+    let aboutTab = document.querySelector("#tab--about");
+    let portfolioTab = document.querySelector("#tab--portfolio");
+
+    aboutTab.addEventListener("click", function (event) {
+        showAbout();
+    })
+
+    portfolioTab.addEventListener("click", function (event) {
+        showPortfolio();
+    })
+
+});
