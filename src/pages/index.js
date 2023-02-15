b290710fa3ac704d6e6a1c15a0a72d4d42476615
ib-rim <ib.bashir@hotmail.com>
2023-02-15 11:32:46 +0000

    Moved skills to separate section on project display

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,8 @@ export default function Home() {
     const [projects] = useState([
         {
             title: "GiveMeSport World Cup 2022",
-            description: <>Special Qatar 2022 design on <a href='https://www.givemesport.com'>GiveMeSport</a> that displays only on World Cup related pages using <span className='u-highlight u-sass'>Sass</span> and <span className='u-highlight u-php'>PHP</span>.</>,
+            description: <>Special Qatar 2022 design on <a href='https://www.givemesport.com'>GiveMeSport</a> that displays only on World Cup related pages.</>,
+            skills: ["Sass", "PHP"],
             src: "/assets/img/gms-worldcuphub.png",
             alt: "The World Cup 2022 hub page for GiveMeSport which uses maroon with background text reading 'Qatar' and custom created images showing famous footballers.",
             gitLink: "",
@@ -15,7 +16,8 @@ export default function Home() {
         },
         {
             title: "Latest Articles Chrome Extension",
-            description: <>Chrome extension that retrieves the latest articles and quizzes from <a href='https://www.videocelts.com'>Videocelts</a> by calling the WordPress REST API. Created for <a href='https://www.snack-media.com'>Snack Media</a> using <span className='u-highlight u-react'>React</span> and <span className='u-highlight u-sass'>Sass</span>.</>,
+            description: <>Chrome extension that retrieves the latest articles and quizzes from <a href='https://www.videocelts.com'>Videocelts</a> by calling the WordPress REST API. Created for <a href='https://www.snack-media.com'>Snack Media</a>.</>,
+            skills: ["React", "Sass"],
             src: "/assets/img/snackextension.png",
             alt: "List of article names with how long ago they were published underneath. The currently selected tab is Latest News and the unselected tab is Latest Quizzes.",
             gitLink: "",
@@ -23,7 +25,8 @@ export default function Home() {
         },
         {
             title: "Mighty Morphle",
-            description: <>Power Rangers themed Wordle clone created with vanilla <span className='u-highlight u-html'>HTML</span>, <span className='u-highlight u-css'>CSS</span> and <span className='u-highlight u-js'>JS</span>. The goal is to guess the random power ranger each day using the given clues.</>,
+            description: <>Power Rangers themed Wordle clone. The goal is to guess the random power ranger each day using the given clues.</>,
+            skills: ["HTML", "CSS", "JS"],
             src: "/assets/img/mightymorphle.png",
             alt: "Four by seven grid displaying attempts at guessing the correct Power Ranger for the day. Columns display the guessed rangers' name, colour, season and ownership era. Correct elements have a green border while wrong elements have a red border. The correct answer was Tyler Navarro, the Red Dino Charge ranger from the Neo Saban Era.",
             gitLink: "https://github.com/ib-rim/power-ranger-wordle",
@@ -31,7 +34,8 @@ export default function Home() {
         },
         {
             title: "CFOP Speedcubing Web App",
-            description: <>Speedcubing app that teaches you how to solve a Rubik&apos;s cube using the CFOP method. This was created using <span className='u-highlight u-react'>React</span>, <span className='u-highlight u-django'>Django</span> and <span className='u-highlight u-unity'>Unity</span> as part of my final year undergraduate project at university.</>,
+            description: <>Speedcubing app that teaches you how to solve a Rubik&apos;s cube using the CFOP method. This was created as part of my final year undergraduate project at university.</>,
+            skills: ["Unity", "Django", "React"],
             src: "/assets/img/cfop.png",
             alt: "Page with three columns, the first two are used for selecting the type of algorithm (PLL and Corners Only are currently selected). The Aa Perm, Ab Perm and E Perm algorithms are shown in the third column.",
             gitLink: "https://github.com/ib-rim/learn-CFOP",
@@ -39,7 +43,8 @@ export default function Home() {
         },
         {
             title: "Card Games Web App",
-            description: <>Created with <span className='u-highlight u-nextjs'>NextJS</span>. Currently only has a singleplayer blackjack game where the goal is to get the value of your hand as close to 21 as possible without going over. If the value goes over 21, you are bust and you lose. Other card games are in progress.</>,
+            description: <>Currently only has a singleplayer blackjack game where the goal is to get the value of your hand as close to 21 as possible without going over. If the value goes over 21, you are bust and you lose. Other card games are in progress.</>,
+            skills: ["NextJS"],
             src: "/assets/img/blackjack.png",
             alt: "The hand is Eight of Hearts and Ace of Clubs with options to Hit, Stand and Reset.",
             gitLink: "https://github.com/ib-rim/card-games",
@@ -47,7 +52,8 @@ export default function Home() {
         },
         {
             title: "The Four Trials",
-            description: <>2D platforming game created using <span className='u-highlight u-unity'>Unity2D</span> and <span className='u-highlight u-c-sharp'>C#</span>. Made over the course of 12 weeks as part of the Multi-Platform Games Development module at Queen Mary University of London. I worked on all aspects of the game including game mechanics, level design, lighting, audio, UI design and story. The final submission of the project was graded 100%.</>,
+            description: <>2D platforming game made over the course of 12 weeks as part of the Multi-Platform Games Development module at Queen Mary University of London. I worked on all aspects of the game including game mechanics, level design, lighting, audio, UI design and story. The final submission of the project was graded 100%.</>,
+            skills: ["Unity", "C#"],
             src: "/assets/img/fourtrials.png",
             alt: "The starting menu of the game which displays Play, Options, Controls and Exit.",
             gitLink: "https://github.com/ib-rim/escape-platformer",
@@ -55,7 +61,8 @@ export default function Home() {
         },
         {
             title: "Image Reveal WordPress Plugin",
-            description: <>WordPress plugin created for <a href='https://www.snack-media.com'>Snack Media</a> that allows for an image guessing game to be embedded into an article. Fully customisable from the WordPress backend. Created using <span className='u-highlight u-react'>React</span>, <span className='u-highlight u-sass'>Sass</span> and <span className='u-highlight u-php'>PHP</span>.</>,
+            description: <>WordPress plugin created for <a href='https://www.snack-media.com'>Snack Media</a> that allows for an image guessing game to be embedded into an article. Fully customisable from the WordPress backend.</>,
+            skills: ["React", "Sass", "PHP"],
             src: "/assets/img/imagereveal.png",
             alt: "Three by three grid of blue squares where the middle right, bottom left and bottom center squares have been revealed showing parts of a Manchester United jersey from the image to be guessed.",
             gitLink: "",
@@ -174,6 +181,11 @@ export default function Home() {
                             <section className={`project__content ${animation}`}>
                                 <h2 className="project__title">{projects[currentProject].title}</h2>
                                 <p className="project__description">{projects[currentProject].description}</p>
+                                <div className="project__skills">
+                                    {projects[currentProject].skills.map((skill) => (
+                                        <span key={skill} className={`u-highlight u-${skill === "C#" ? 'c-sharp' : skill.toLowerCase()}`}>{skill}</span>
+                                    ))}
+                                </div>
                                 <div className="project__links">
                                     {
                                         projects[currentProject].gitLink ?
